da0b914697bf9432fe102fb9a1a5adbedb122b60
Brandon Harroff <brandoh@gmail.com>
2019-10-13 20:17:36 -0500

    fixes to vpc

--- modules/private/network/vpc/main.tf
+++ modules/private/network/vpc/main.tf
@@ -1,10 +1,10 @@
 # base aws_vpc module
 
 resource "aws_vpc" "vpc" {
-  cidr_block       = cidr_block
+  cidr_block       = "${cidr_block}"
   enable_dns_hostnames = "true"
-  
+
   tags = {
-    Name = vpc_name
+    Name = "${aws_vpc.vpc}"
   }
 }
